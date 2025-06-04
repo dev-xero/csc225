7b16b9fb8427a6a7ea4be9ec9380997078aae3f1
= <ssh.xero@gmail.com>
2025-06-04 20:55:15 +0100

    update: bisection algorithm
    Signed-off-by: = <ssh.xero@gmail.com>

--- bisection.py
+++ bisection.py
@@ -1,3 +1,7 @@
+import math
+import inspect
+
+
 def bisection(f, a, b, epsilon):
     """
         Bisection is a root-finding method for continuous functions
@@ -19,39 +23,64 @@ def bisection(f, a, b, epsilon):
     """
     fa, fb = f(a), f(b)
 
+    if abs(fa) < epsilon:
+        return a
+    if abs(fb) < epsilon:
+        return b
     if fa * fb > 0:
-        return -1
+        raise Exception("Interval does not cross 0")
 
     c = (a + b) / 2
     fc = f(c)
 
-    if abs(fc) < epsilon:
+    if abs(fc) < epsilon or abs(b - a) < epsilon:
         return c
 
-    if abs(b - a) < epsilon:
-        return c
-
-    elif samesign(fa, fc):
+    if fa * fc > 0:
         return bisection(f, c, b, epsilon)
-
-    elif samesign(fb, fc):
+    else:
         return bisection(f, a, c, epsilon)
 
 
-def samesign(x, y):
-    return (x > 0 and y > 0) or (x < 0 and y < 0)
+def source(fx):
+    """
+        Helper function to get lambda source text
+    """
+    text = inspect.getsource(fx)
+    text = text.strip()
+    text = text.split("lambda x: ")[1]
+    return text
 
 
-def main():
-    header = "BISECTION TEST CASES:"
-    print(header)
-    print("-" * len(header))
+if __name__ == "__main__":
+    test_cases = [
+        {"fx": lambda x: 1 - (2 * x * math.pow(math.e, -x/2)),
+         "a": 0.1, "b": 2},
+        {"fx": lambda x: 5 - math.pow(x, -1), "a": 0.1, "b": 0.3},
+        {"fx": lambda x: math.pow(x, 3) - (2 * x) - 5, "a": 2, "b": 3},
+        {"fx": lambda x: math.pow(math.e, x) - 2, "a": 0, "b": 2},
+        {"fx": lambda x: x - math.pow(math.e, -x), "a": 0, "b": 1},
+        {"fx": lambda x: math.pow(x, 6) - x - 1, "a": 1, "b": 2},
+        {"fx": lambda x: math.pow(x, 2) - math.sin(x), "a": 0, "b": 1},
+        {"fx": lambda x: math.pow(x, 3) - 2, "a": 1, "b": 2},
+        {"fx": lambda x: x + math.tan(x), "a": -1, "b": 0},
+        # {"fx": lambda x: 2 - (math.pow(x, -1) * math.log(x)), "a": 0.01, "b": 2},
+        # the above can't be solved with bisection, tsk tsk
+    ]
 
-    def f1(x):
-        return 2*x**2 - 7*x + 6
+    EPSILON = 1e-6
+    print("FINDING ROOTS USING BISECTION METHOD")
+    for idx, test in enumerate(test_cases):
+        fx = test["fx"]
+        a = test["a"]
+        b = test["b"]
 
-    root = bisection(f1, 1, 2, 1e-6)
-    print(f"- Root found: {root}")
+        print("-" * 55)
+        print(f"Problem {chr(65 + idx)}")
+        print("-" * 55)
+        print(f"- f(x): {source(fx)}")
+        print(f"- a: {a}")
+        print(f"- b: {b}")
 
-
-main()
+        root = bisection(fx, a, b, EPSILON)
+        print(f"- found root as: {root}\n")
