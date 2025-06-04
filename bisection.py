016ec35a294d110dd8c5ffa17ec3232728be1031
= <ssh.xero@gmail.com>
2025-06-04 19:12:53 +0100

    update: bisection algorithm
    Signed-off-by: = <ssh.xero@gmail.com>

--- bisection.py
+++ bisection.py
@@ -1,34 +1,36 @@
-"""
------------------------------------------------------------
-Bisection algorithm
------------------------------------------------------------
-Basically a root-finding method for continuous functions
-where we halve the search space/interval [a,b] continuously
-within a tolerance factor, epsilon.
-
-The intervals are chosen such that there exists 0 between
-them i.e. a < c < b, for each iteration we move in the
-direction that preserves this order.
-
-In essence, we are just performing binary search on real
-numbers within a tolerance threshold.
------------------------------------------------------------
-"""
 def bisection(f, a, b, epsilon):
+    """
+        Bisection is a root-finding method for continuous functions
+        where we halve the search space/interval [a,b] continuously
+        within a tolerance factor, epsilon
+
+        the intervals are chosen such that there exists 0 between
+        them i.e. a < c < b, for each iteration we move in the
+        direction that preserves this order
+
+        in essence, we are just performing binary search on real
+        numbers within a tolerance threshold
+
+        parameters:
+        - f: function
+        - a: first value of the interval
+        - b: second value of the interval
+        - epsilon: tolerance threshold
+    """
     fa, fb = f(a), f(b)
-    
+
     if fa * fb > 0:
         return -1
 
     c = (a + b) / 2
     fc = f(c)
-    
+
     if abs(fc) < epsilon:
         return c
 
     if abs(b - a) < epsilon:
         return c
-    
+
     elif samesign(fa, fc):
         return bisection(f, c, b, epsilon)
 
@@ -39,17 +41,17 @@ def bisection(f, a, b, epsilon):
 def samesign(x, y):
     return (x > 0 and y > 0) or (x < 0 and y < 0)
 
+
 def main():
     header = "BISECTION TEST CASES:"
     print(header)
     print("-" * len(header))
 
-    def f1(x): 
+    def f1(x):
         return 2*x**2 - 7*x + 6
-    
+
     root = bisection(f1, 1, 2, 1e-6)
     print(f"- Root found: {root}")
 
+
 main()
-
-
