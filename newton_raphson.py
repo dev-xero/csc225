f19da4ce4a639fe5f7352d24e9243e39bcbb587f
= <ssh.xero@gmail.com>
2025-06-04 19:07:45 +0100

    add: implement newton-raphson algorithm
    Signed-off-by: = <ssh.xero@gmail.com>

--- newton_raphson.py
+++ newton_raphson.py
@@ -1,6 +1,7 @@
 import math
 import inspect
 
+
 def newton_raphson(fx, fx_prime, x0, epsilon, max_iter=100, iteration=0):
     """
         Newton Raphson's method is a root finding algorithm
@@ -12,7 +13,7 @@ def newton_raphson(fx, fx_prime, x0, epsilon, max_iter=100, iteration=0):
         - fx_prime: the derivative of the function, fx
         - x0: initial guess
         - epsilon: tolerance threshold
-        
+
         formula:
         - x1 = x0 - fx/fx_prime
 
@@ -24,15 +25,16 @@ def newton_raphson(fx, fx_prime, x0, epsilon, max_iter=100, iteration=0):
     f, fp = fx(x0), fx_prime(x0)
     if fp == 0:
         raise Exception("Cannot divide by zero")
-    
+
     if abs(fp) < epsilon:
         raise Exception("Derivative too small")
-    
+
     x1 = x0 - f/fp
     if abs(x1 - x0) < epsilon:
         return x1
 
     return newton_raphson(fx, fx_prime, x1, epsilon, max_iter, iteration + 1)
+
 
 def source(fx):
     """
@@ -43,7 +45,9 @@ def source(fx):
     text = text.split("lambda x: ")[1]
     return text
 
+
 if __name__ == "__main__":
+    # note: sec(x) replaced with 1/cos(x)
     test_cases = [
         {
             "fx": lambda x: 1 - (2 * x * math.pow(math.e, -x/2)),
@@ -61,19 +65,62 @@ if __name__ == "__main__":
             "fx": lambda x: math.pow(x, 3) - (2 * x) - 5,
             "fx_prime": lambda x: 3 * math.pow(x, 2) - 2,
             "x0": 2
-        }
+        },
+
+        {
+            "fx": lambda x: math.pow(math.e, x) - 2,
+            "fx_prime": lambda x: math.pow(math.e, x),
+            "x0": 1
+        },
+
+        {
+            "fx": lambda x: x - math.pow(math.e, -x),
+            "fx_prime": lambda x: 1 + math.pow(math.e, -x),
+            "x0": 1
+        },
+
+        {
+            "fx": lambda x: math.pow(x, 6) - x - 1,
+            "fx_prime": lambda x: 6 * math.pow(x, 5) - 1,
+            "x0": 1
+        },
+
+        {
+            "fx": lambda x: math.pow(x, 2) - math.sin(x),
+            "fx_prime": lambda x: (2 * x) - math.cos(x),
+            "x0": 1/2
+        },
+
+        {
+            "fx": lambda x: math.pow(x, 3) - 2,
+            "fx_prime": lambda x: 3 * math.pow(x, 2),
+            "x0": 1
+        },
+
+        {
+            "fx": lambda x: x + math.tan(x),
+            "fx_prime": lambda x: 1 + math.pow(1/math.cos(x), 2),
+            "x0": 3
+        },
+
+        {
+            "fx": lambda x: 2 - (math.pow(x, -1) * math.log(x)),
+            "fx_prime": lambda x: (1 - math.log(x)) / math.pow(x, 2),
+            "x0": 1/3
+        },
     ]
-    
+
     EPSILON = 1e-6
-    
+
     print("SOLVING EQUATIONS USING NEWTON-RAPHSON METHOD\n")
     for idx, test in enumerate(test_cases):
         fx = test["fx"]
         fx_prime = test["fx_prime"]
-        x0 = test["x0"]  
+        x0 = test["x0"]
 
-        print(f"{chr(65 + idx)}")
-        print("-" * 35)
+        print("-" * 55)
+        print(f"Problem {chr(65 + idx)}")
+        print("-" * 55)
         print(f"- f(x): {source(fx)}")
         print(f"- f'(x): {source(fx_prime)}")
         print(f"- x0: {x0}")
@@ -82,4 +129,4 @@ if __name__ == "__main__":
             root = newton_raphson(fx, fx_prime, x0, EPSILON)
             print(f"- found root as: {root}\n")
         except Exception as e:
-            print(f"- error: {e}")
+            print(f"- error: {e}\n")
